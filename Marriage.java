a25596a6cf60758669c897de711a1b4f3c71a494
Will Haack <wmh1993@gmail.com>
2011-05-16 19:35:24 -0700

    Edited Marriage.java via GitHub

--- Marriage.java
+++ Marriage.java
@@ -1,45 +1,35 @@
 import java.util.*;
-
-public class Man {
-
-    private String name;
-    private ArrayList<Woman> partners;
+public class Marriage implements Comparable {
+    private Man guy;
+    private Woman girl;
     
-    public Man(String name){
-        this.name = name;
-        partners = new ArrayList<Woman>();
+    public Marriage(Man dude, Woman chick){
+        guy = dude;
+        girl = chick;
     }
     
     public String toString(){
-        return name;
-    }
-    
-    public void setPartners(ArrayList<Woman> partners){
-        this.partners = partners;
-    }
-    
-    public Man(String name, ArrayList<Woman> list) {
-        this.name = name;
-        partners = list;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public boolean equals(Object other) {
-        return getName().equals(((Man) other).getName());
-    }
-    
-    public Woman getWoman(int i){
-        return partners.get(i);
+        return guy.getName() + " " + girl.getName();
     }
     
     public int compareTo(Object other){
-        return getName().compareTo(((Man)other).getName());
+        if (getMan().equals(((Marriage)other).getMan()) || getWoman().equals(((Marriage)other).getWoman()))
+            return 0;
+        return 1;
     }
     
-    public void addWoman(Woman w){
-        partners.add(w);
+    public Man getMan(){
+        return guy;
     }
+    
+    public Woman getWoman(){
+        return girl;
+    }
+    
+    public boolean equals(Object other){
+        return getMan().equals(((Marriage)other).getMan()) || getWoman().equals(((Marriage)other).getWoman());
+    }
+    
+    
+    
 }
