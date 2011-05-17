fda84267ec95efdf1b8dbd2e4621e5c59bcbe2e6
Will Haack <wmh1993@gmail.com>
2011-05-16 19:34:28 -0700

    Edited Marriage.java via GitHub

--- Marriage.java
+++ Marriage.java
@@ -1,24 +1,35 @@
 import java.util.*;
-public class Marriage{
- 
-  private Man husband;
-  private Woman wife;
- 
-  public Marriage(Man x, Woman y){
-    husband = x;
-    wife = y;
-  }
- 
-  public Man getHusband() {
-    return husband;
-  }
- 
-  public Woman getWife(){
-    return wife;
-  }
- 
-  public boolean equals(Object other){
-    return ((Marriage) other).getHusband() == husband && ((Marraige) other).getWife() == wife;
-  }
- 
+public class Marriage implements Comparable {
+    private Man guy;
+    private Woman girl;
+    
+    public Marriage(Man dude, Woman chick){
+        guy = dude;
+        girl = chick;
+    }
+    
+    public String toString(){
+        return guy.getName() + " " + girl.getName();
+    }
+    
+    public int compareTo(Object other){
+        if (getMan().equals(((Marriage)other).getMan()) || getWoman().equals(((Marriage)other).getWoman()))
+            return 0;
+        return 1;
+    }
+    
+    public Man getMan(){
+        return guy;
+    }
+    
+    public Woman getWoman(){
+        return girl;
+    }
+    
+    public boolean equals(Object other){
+        return getMan().equals(((Marriage)other).getMan()) || getWoman().equals(((Marriage)other).getWoman());
+    }
+    
+    
+    
 }
