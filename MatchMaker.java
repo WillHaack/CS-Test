7cbb1bb1278920d34b3dd6f41851354559dc11b6
Will Haack <wmh1993@gmail.com>
2011-05-16 19:33:41 -0700

    Edited MatchMaker.java via GitHub

--- MatchMaker.java
+++ MatchMaker.java
@@ -1,110 +1,82 @@
-import java.util.*;
 
-public class MatchMaker{
-  
-  private ArraySet<Man> men;
-  private ArraySet<Woman> women;
-  private ArraySet<Marriage> propMars;
-  private ArraySet<Human> marriages;
-  
-  public MatchMaker(ArrayList<Man> x, ArraySet<Woman y){
-    men = x;
-    women = y;
-    propMars = new ArraySet<Marraige>();
-  }
-  
-   public void match(){
-     int cycle = 0;
-     while (! isDone()){
-       for (Man m: men){
-         if (! hasPartnerM(m)) {
-           Woman current = m.getPref().get(cycle);
-           propose(m, current);
-         }
-       }
-       for (Woman w: women){
-         womanDecision(w);
-         if (w.numProp() > 0) {
-           Man husband = w.getProp().get(0);
-           w.setPartner(husband);
-           marriages.add(w);
-           marriages.add(husband);
-         }
-       }
-       cycle++;
-     }
-   }
-          
-           
-       
-       
-     }
-   
-   }
-  
-  
-  // if woman has more than one proposals, she rejects all but the best
-  private void womanDecision(Woman x){
-    if (x.numProp() > 1){
-      Man bestMan = x.getProp().get(0);
-      int max = x.getRankOfMan(bestMan);
-      
-      for (Man m: x.getProp()){
-        if (x.getRankOfMan(m) < max){
-          bestMan = m;
-          max = x.getRankOfMan(bestMan);
+import java.util.*;
+import java.io.*;
+public class MatchMaker {
+
+    public static Set<Marriage> generator(Set<Man> men, Set<Woman> women) {
+        Set<Marriage> ans = new ArraySet<Marriage>();
+        for (int i = 0; i < women.size(); i++) {
+            Iterator itr = men.iterator();
+            while (itr.hasNext()) {
+                Man temp = ((Man) itr.next());
+                for (int r = 0; r <= i; r++) {
+                    if (temp.equals(temp.getWoman(i).getMan(r))) {
+                        ans.add(new Marriage(temp, temp.getWoman(r)));
+                    }
+                }
+            }
         }
-        else {
-          reject(x, m);
-        }
-      }
-    }
-  }
-  
-  // see if match making process is done
-  private boolean isDone(){
-    boolean ans = true;
-    for (Man x: men)
-      if (! x.isMarried()) return false;
-    
-    return ans;
-  }
-  
-  
-// Woman x rejects Man y
-  private void reject(Woman x, Man y){
-    y.setPartner(null);
-    x.removeProp(y);
-    propMars.remove(getPosOfMarriage(y, x));
-  }
-  
-// gets position of the marriage of man x and woman y in the temp array
-  private int getPosOfMarriage(Man x, Woman y){
-    int ans = 0;
-    for (Marriage mar: temp){
-      if (mar.getWife().equals(y) && mar.getHusband().equals(x))
         return ans;
-      else ans++;
     }
-    return -1;
-  }
-  
-  // Man x proposes to Woman y. 
-  private void propose(Man m, Woman w){
-    m.setPartner(w);
-    w.addProp(m);
-    propMars.add(new Marriage(m, w));
-  }
-  
-  // returns true is Man x has a partner
-  private boolean hasPartnerM(Man x){
-    return x.isMarried();
-  }
-  
-  //returns true if Woman x has a partner
-  private boolean hasPartnerF(Woman x){
-    return x.isMarried();
-  }
-  
- 
+    
+    public static Woman getWoman(String name, ArrayList<Woman> women){
+      for (int i = 0; i < women.size(); i++){
+        if (name.equals(women.get(i).getName()))
+          return women.get(i);
+      }
+      System.out.println("error");
+    return null;
+    }
+    
+    public static Man getMan(String name, ArrayList<Man> men){
+      for (int i = 0; i < men.size(); i++){
+        if (name.equals(men.get(i).getName()))
+          return men.get(i);
+      }
+      System.out.println("error");
+      return null;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader f = new BufferedReader(new FileReader("data.txt"));
+        ArrayList<Man> men = new ArrayList<Man>();
+        ArrayList<Woman> women = new ArrayList<Woman>();
+        int num_people = Integer.parseInt(f.readLine());
+        StringTokenizer get_men = new StringTokenizer(f.readLine());
+        StringTokenizer get_women = new StringTokenizer(f.readLine());
+        while (get_men.hasMoreTokens()){
+            men.add(new Man(get_men.nextToken()));
+        }
+        while (get_women.hasMoreTokens()){
+            women.add(new Woman(get_women.nextToken()));
+        }
+        for (int i = 0; i < men.size(); i++){
+            Man my_man = getMan(f.readLine(), men);
+            ArrayList<Woman> pref = new ArrayList<Woman>();
+            StringTokenizer st = new StringTokenizer(f.readLine());
+            for (int q = 0; q < women.size(); q++){
+                pref.add(getWoman(st.nextToken(), women));
+            }
+            my_man.setPartners(pref);
+        }
+        for (int i = 0; i < men.size(); i++){
+            Woman my_woman = getWoman(f.readLine(), women);
+            ArrayList<Man> pref = new ArrayList<Man>();
+            StringTokenizer st = new StringTokenizer(f.readLine());
+            for (int q = 0; q < men.size(); q++){
+                pref.add(getMan(st.nextToken(), men));
+            }
+            my_woman.setPartners(pref);
+        }
+        ArraySet<Man> menset = new ArraySet<Man>();
+        ArraySet<Woman> womenset = new ArraySet<Woman>();
+        for (int i = 0; i < men.size(); i++){
+          menset.add(men.get(i));
+          womenset.add(women.get(i));
+        }
+        Iterator itr = generator(menset, womenset).iterator();
+        while (itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
 }
